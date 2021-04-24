413c7121c176e1bc1e0b730746e8c1208ff2c5f7
D34TH5TR1K3 <77392455+D34TH5TR1K3@users.noreply.github.com>
2021-04-24 21:18:33 +0200

    Made playermotion smoother by incrementing velocity over time

--- src/shooter/entities/Player.java
+++ src/shooter/entities/Player.java
@@ -5,7 +5,8 @@ import shooter.Handler;
 import shooter.gfx.Assets;
 
 public class Player extends Entity{
-    private final int SPEED = 8;
+    private int velX = 0, velY = 0;   //velocity in x and y direction
+    private int velXmax = 10, velYmax = 10;   //maximum velocity
     private Item gun = null;
 
     public Player(int posX, int posY, int width, int height, Handler handler) {
@@ -14,16 +15,37 @@ public class Player extends Entity{
 
     @Override
     public void tick() {
-        move(((handler.getKeyManager().right)?SPEED:0)-((handler.getKeyManager().left)?SPEED:0),
-                ((handler.getKeyManager().down)?SPEED:0)-((handler.getKeyManager().up)?SPEED:0));
+        if(handler.getKeyManager().right && velX > -velXmax){
+            velX -= 1;
+        }else if(!handler.getKeyManager().right && !handler.getKeyManager().left){
+            velX = 0;
+        }
+        if(handler.getKeyManager().left && velX < velXmax){
+            velX += 1;
+        }else if(!handler.getKeyManager().right && !handler.getKeyManager().left){
+            velX = 0;
+        }
+        if(handler.getKeyManager().down && velY > -velYmax){
+            velY -= 1;
+        }else if(!handler.getKeyManager().down && !handler.getKeyManager().up){
+            velY = 0;
+        }
+        if(handler.getKeyManager().up && velY < velYmax){
+            velY += 1;
+        }else if(!handler.getKeyManager().down && !handler.getKeyManager().up){
+            velY = 0;
+        }
+        move(velX, velY);
+        /*move(((handler.getKeyManager().right)?SPEED:0)-((handler.getKeyManager().left)?SPEED:0),
+                ((handler.getKeyManager().down)?SPEED:0)-((handler.getKeyManager().up)?SPEED:0));*/
         //TODO implement Handler
         //TODO player collision
         handler.getGameCamera().centerOnEntity(this);
     }
     @Override
     public void render(Graphics g) {
-        System.out.println("Position"+posX+"\t"+posY);
-        System.out.println("Offset"+handler.getxOffset()+"\t"+ handler.getyOffset());
+        //System.out.println("Position"+posX+"\t"+posY);
+        //System.out.println("Offset"+handler.getxOffset()+"\t"+ handler.getyOffset());
         g.setColor(Color.DARK_GRAY);
         g.fillRect(10,10,20,20);
         g.drawImage(Assets.player, (int)(posX-handler.getxOffset()), (int)(posY-handler.getyOffset()), Entity.CREATURESIZE, Entity.CREATURESIZE, null);
